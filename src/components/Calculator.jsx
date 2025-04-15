3e5097d7a72cdf051d031a33f463409da4077d29
Pavithr Jain <pavithrjain19@gmail.com>
2025-04-15 20:00:14 +0530

    changed

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -124,15 +124,15 @@ const Calculator = () => {
     }, [handleClick]);
 
     return (
-        <div className={`flex flex-col gap-4 items-center justify-center min-h-screen p-4 transition-all duration-500 ${theme === 'dark' ? 'bg-gray-900 text-white' : 'bg-gray-100 text-gray-900'}`}>
+        <div className={`flex flex-col gap-4 items-center justify-center min-h-screen p-4 transition-all duration-500 ${theme === "dark" ? "bg-gray-900 text-white" : "bg-gray-100 text-gray-900"}`}>
             {warning &&
                 <div className="absolute z-10 px-4 py-1 text-sm text-white transition-all duration-500 bg-yellow-500 rounded shadow-lg w-fit top-2 animate-fadeInUp">
                     Max input length reached!
                 </div>
             }
 
-            <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')} className={`absolute z-10 p-2 text-xl transition rounded-full shadow-2xl top-2 right-2 sm:text-sm ${theme === 'dark' ? "text-white border-gray-400" : "text-black border"}`}>
-                {theme === 'dark' ? <Sun /> : <Moon />}
+            <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")} className={`absolute z-10 p-2 text-xl transition rounded-full shadow-2xl top-2 right-2 sm:text-sm ${theme === "dark" ? "text-white border-gray-400" : "text-black border"}`}>
+                {theme === "dark" ? <Sun /> : <Moon />}
             </button>
 
             <div className="relative flex flex-col w-full max-w-sm gap-4 p-5 shadow-2xl bg-white/10 backdrop-blur-md rounded-2xl">
