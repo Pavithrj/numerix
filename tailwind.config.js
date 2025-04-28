7068c24a6bca020e0f156e40e14cc1abdfb95947
Pavithr jain <68775236+Pavithrj@users.noreply.github.com>
2025-04-28 10:31:31 +0000

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,20 +13,20 @@ export default {
                 fadeInUp: "fadeInUp 0.4s ease-out",
                 fadeIn: "fadeIn 1s ease-in-out",
                 fadeInDelayed: "fadeIn 1s ease-in-out 0.3s backwards",
-                bounceSlow: "bounce 3s infinite",
+                bounceSlow: "bounce 3s infinite"
             },
             keyframes: {
                 fadeInOut: {
                     "0%, 100%": { opacity: "0" },
-                    "10%, 90%": { opacity: "1" },
+                    "10%, 90%": { opacity: "1" }
                 },
                 fadeInUp: {
                     "0%": { opacity: "0", transform: "translateY(10px)" },
-                    "100%": { opacity: "1", transform: "translateY(0)" },
+                    "100%": { opacity: "1", transform: "translateY(0)" }
                 },
                 fadeIn: {
                     "0%": { opacity: 0, transform: "translateY(-10px)" },
-                    "100%": { opacity: 1, transform: "translateY(0)" },
+                    "100%": { opacity: 1, transform: "translateY(0)" }
                 }
             }
         }
