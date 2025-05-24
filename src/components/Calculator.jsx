ece084bbbdb257cadaf9ac750b2f20d410018ec3
Pavithr jain <68775236+Pavithrj@users.noreply.github.com>
2025-05-24 05:44:52 +0000

    change

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -124,7 +124,7 @@ const Calculator = () => {
     }, [handleClick]);
 
     return (
-        <div className={`flex flex-col gap-4 items-center justify-center min-h-screen p-4 transition-all duration-500 ${theme === "dark" ? "bg-gray-900 text-white" : "bg-gray-100 text-gray-900"}`}>
+        <main className={`flex flex-col gap-4 items-center justify-center min-h-screen p-4 transition-all duration-500 ${theme === "dark" ? "bg-gray-900 text-white" : "bg-gray-100 text-gray-900"}`}>
             {warning &&
                 <div className="absolute z-10 px-4 py-1 text-sm text-white transition-all duration-500 bg-yellow-500 rounded shadow-lg w-fit top-2 animate-fadeInUp">
                     Max input length reached!
