63ed695cb1363c42973a89a7030ed4f185b092e7
Pavithr jain <68775236+Pavithrj@users.noreply.github.com>
2025-05-24 05:58:35 +0000

    change

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -172,7 +172,7 @@ const Calculator = () => {
                 <div className="flex flex-col w-full max-w-sm p-5 shadow-2xl bg-white/10 backdrop-blur-md rounded-2xl">
                     <ul className="flex flex-col justify-between gap-2 text-sm">
                         {history.map((item, index) => (
-                            <li key={index} onClick={() => handleHistoryClick(item.expression)} className={`flex justify-between transition-all duration-500 rounded cursor-pointer hover:bg-white/20 animate-fadeInUp ${theme === 'dark' ? "text-gray-300" : "text-black"}`}>
+                            <li key={index} onClick={() => handleHistoryClick(item.expression)} className={`flex justify-between transition-all duration-500 rounded cursor-pointer hover:bg-white/20 animate-fadeInUp ${theme === "dark" ? "text-gray-300" : "text-black"}`}>
                                 <div>
                                     {item.expression}
                                 </div>
