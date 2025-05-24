5d576a6ae50b8e1d96fc1396bd813a8540591acf
Pavithr jain <68775236+Pavithrj@users.noreply.github.com>
2025-05-24 05:57:40 +0000

    change

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -157,7 +157,7 @@ const Calculator = () => {
             <div className="flex w-full max-w-sm justify-evenly">
                 {history.length > 0 && (
                     <button onClick={() => setShowHistory(!showHistory)} className="px-4 py-1 text-white transition bg-gray-700 rounded hover:bg-gray-600">
-                        {showHistory ? 'Hide History' : 'Show History'}
+                        {showHistory ? "Hide History" : "Show History"}
                     </button>
                 )}
 
