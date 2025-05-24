a58916c9ac23834969dcd766dff604d4975d4f82
Pavithr jain <68775236+Pavithrj@users.noreply.github.com>
2025-05-24 06:28:10 +0000

    change

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -2,6 +2,7 @@ import { useState, useRef, useEffect, useCallback } from 'react';
 import { evaluate } from 'mathjs';
 import { Sun, Moon } from 'lucide-react';
 import WarningMessage from './WarningMessage';
+import HistoryButtons from './HistoryButtons';
 
 const buttons = [
     ["C", "Del", "%", "/"],
@@ -153,19 +154,7 @@ const Calculator = () => {
                 </div>
             </div>
 
-            <div className="flex w-full max-w-sm justify-evenly">
-                {history.length > 0 &&
-                    <button onClick={() => setShowHistory(!showHistory)} className="px-4 py-1 text-white transition bg-gray-700 rounded hover:bg-gray-600">
-                        {showHistory ? "Hide History" : "Show History"}
-                    </button>
-                }
-
-                {history.length > 0 && showHistory &&
-                    <button onClick={clearHistory} className="px-4 py-1 text-white transition rounded bg-rose-600 hover:bg-rose-700">
-                        Clear History
-                    </button>
-                }
-            </div>
+            <HistoryButtons history={history} setShowHistory={setShowHistory} showHistory={showHistory} clearHistory={clearHistory} />
 
             {showHistory && history.length > 0 &&
                 <div className="flex flex-col w-full max-w-sm p-5 shadow-2xl bg-white/10 backdrop-blur-md rounded-2xl">
